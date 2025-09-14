a6b6d31a52e43705a9177a1b4309d2b4039f9e3d
Megumi Kishibe <Kishibe0417@gmail.com>
2025-09-14 23:34:35 +0900

    Implement update action for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 root "articles#index"
 
-  resources :articles, only: [:index, :show, :new, :edit, :create]
+  resources :articles, only: [:index, :show, :new, :edit, :create, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
