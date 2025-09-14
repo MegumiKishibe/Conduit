6e658be7d2ae71e0eedb7aae26dde26460d476bd
Megumi Kishibe <Kishibe0417@gmail.com>
2025-09-14 23:59:01 +0900

    Add delete functionality for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 root "articles#index"
 
-  resources :articles, only: [:index, :show, :new, :edit, :create, :update]
+  resources :articles, only: [:index, :show, :new, :edit, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
